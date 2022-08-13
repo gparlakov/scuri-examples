706ecac28bc129464b32b37e0ff30e826f7b7f32
Georgi Parlakov <gparlakov@gmail.com>
2022-08-13 12:53:19 +0000

    move files to src/app

--- scuri-test-context/src/app/other/other.component.ts
+++ scuri-test-context/src/app/other/other.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Service } from '../service';
+import { take } from 'rxjs/operators';
 
+const captains = console;
 @Component({
     selector: 'app-other',
     templateUrl: './other.component.html',
@@ -11,5 +13,13 @@ export class OtherComponent {
 
     constructor(private s: Service) {
         this.title = s.method() ? this.title + s.method() : this.title;
+    }
+
+    anotherMethod() {
+        this.s.promiseReturning().then(captains.log)
+    }
+
+    method() {
+        this.s.observableReturning().pipe(take(1)).subscribe(v => captains.log('---observable value', v))
     }
 }
